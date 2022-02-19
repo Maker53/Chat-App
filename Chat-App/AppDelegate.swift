06ad2618676505885824799b8d136f3c3d81b46c
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-19 21:59:46 +0300

    feat(AppDelegate): add enum for app states and add one test method (willFinishLaunchingWithOptions)

--- Chat-App/AppDelegate.swift
+++ Chat-App/AppDelegate.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+private enum State: String {
+    case notRunning = "Not Running"
+    case inactive = "Inactive"
+    case active = "Active"
+    case background = "Background"
+    case suspended = "Suspended"
+}
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +22,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        return true
+    }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print(
+            """
+            Application moved from \(State.notRunning.rawValue) state to \(State.inactive.rawValue) state:
+            function - \(#function)
+            """
+        )
+        
         return true
     }
 }
