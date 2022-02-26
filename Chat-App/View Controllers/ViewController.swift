a3b9040ccac6ce7c5ea58518198e2c61752f4d01
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-26 13:53:56 +0300

    initial commit

--- Chat-App/View Controllers/ViewController.swift
+++ Chat-App/View Controllers/ViewController.swift
@@ -7,58 +7,9 @@
 
 import UIKit
 
-// To enable / disable the display of the log in the console,
-// refer to the public variable in the Constants file
-// and assign it the appropriate value (true or false)
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        printLog()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        printLog()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        printLog()
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        printLog()
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        printLog()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        printLog()
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        printLog()
-    }
-    
-    // MARK: - Private Methods
-    private func printLog(method: String = #function) {
-        guard Constants.showLog else { return }
-        
-        print("Method: \(method)\n")
     }
 }
