c061ecffb5de386186ffefe04385d4f6746d25c8
Shalgin Stanislav <s.shalgin@gmail.com>
2022-02-27 13:10:53 +0300

    feat: created UI for Profile View Controller

--- Chat-App/View Controllers/ProfileViewController.swift
+++ Chat-App/View Controllers/ProfileViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        profileImage.layer.cornerRadius = profileImage.frame.width / 2
     }
 }
