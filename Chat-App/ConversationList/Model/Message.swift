f990582a2e420ffd1e64323852d1694d93712f3f
Shalgin Stanislav <s.shalgin@gmail.com>
2022-03-07 22:01:21 +0300

    refactor(Message): refactor model

--- Chat-App/ConversationList/Model/Message.swift
+++ Chat-App/ConversationList/Model/Message.swift
@@ -7,15 +7,53 @@
 
 import Foundation
 
-struct Message {
-    
-    let fullname: String
-    let message: String
-    let date: Date
+struct Status {
+    let online: Bool
+    let message: [Message]
     
     // MARK: - Mock
     static let mock = [
-        Message(fullname: "Maks Petrov", message: "Hello!!!", date: Date()),
-        Message(fullname: "Artyom Federov", message: "Have a nice day", date: Date())
+        Status(
+            online: true,
+            message: [
+                Message(
+                    fullName: "Maks Petrov",
+                    message: "Hello!!!",
+                    date: Date(),
+                    hasUnreadMessages: true
+                ),
+                Message(
+                    fullName: "Artyom Federov",
+                    message: "Have a nice day",
+                    date: Date(),
+                    hasUnreadMessages: false
+                )
+            ]
+        ),
+        Status(
+            online: false,
+            message: [
+                Message(
+                    fullName: "Ivan Petrov",
+                    message: "How are u?",
+                    date: Date(),
+                    hasUnreadMessages: true
+                ),
+                Message(
+                    fullName: "Artyom Bolshakov",
+                    message: "I'm fine",
+                    date: Date(),
+                    hasUnreadMessages: false
+                )
+            ]
+        )
     ]
+}
+
+struct Message {
+    
+    let fullName: String
+    let message: String
+    let date: Date
+    let hasUnreadMessages: Bool
 }
