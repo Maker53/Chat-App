aa0eb5bef83db8fa70a78d4318b67637eec38c5c
Shalgin Stanislav <s.shalgin@gmail.com>
2022-03-07 22:11:19 +0300

    feat(ConversationListViewController): confirm UITableViewDataSource protocol and register cell
    - added UITableViewDataSource protocol methods

--- Chat-App/ConversationList/Controller/ConversationListViewController.swift
+++ Chat-App/ConversationList/Controller/ConversationListViewController.swift
@@ -9,13 +9,56 @@ import UIKit
 
 class ConversationListViewController: UIViewController {
     
-    // MARK: - Private Properties
+    // MARK: - UI
     lazy var conversationListTableView = UITableView(frame: .zero, style: .insetGrouped)
+    lazy var conversationCell = ConversationCell()
+    
+    // MARK: - Private Properties
+    private let messages = Status.mock
     
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupConversationListTableView()
+        
+        conversationListTableView.register(
+            UINib(nibName: String(describing: ConversationCell.self), bundle: nil),
+            forCellReuseIdentifier: ConversationCell.identifier
+        )
+        
+        conversationListTableView.dataSource = self
+    }
+}
+
+extension ConversationListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "Online" : "History"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages[section].message.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: ConversationCell.identifier,
+            for: indexPath
+        )
+        
+        guard let conversationCell = cell as? ConversationCell else { return cell }
+        
+        let status = messages[indexPath.section]
+        let messages = status.message
+        let message = messages[indexPath.row]
+        
+        conversationCell.configure(with: message, and: status)
+        
+        return conversationCell
     }
 }
