e3cfe23aabcd3d69b9239912a1e7c00e347a5211
Shalgin Stanislav <s.shalgin@gmail.com>
2022-03-08 18:40:47 +0300

    refactor(ConversationListViewController)

--- Chat-App/ConversationList/Controller/ConversationListViewController.swift
+++ Chat-App/ConversationList/Controller/ConversationListViewController.swift
@@ -12,8 +12,8 @@ class ConversationListViewController: UIViewController {
     // MARK: - UI
     lazy var conversationListTableView = UITableView(frame: .zero, style: .insetGrouped)
     
-    // MARK: - Private Properties
-    private let users = User.mock
+    // MARK: - Public Properties
+    let users = User.mock
     
     // MARK: - Override Methods
     override func viewDidLoad() {
@@ -27,43 +27,5 @@ class ConversationListViewController: UIViewController {
         )
         
         conversationListTableView.dataSource = self
-    }
-}
-
-extension ConversationListViewController: UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        2
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        section == 0 ? "Online" : "History"
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let onlineUsersCount = users.filter { $0.isOnline }
-        let offlineUsersCount = users.filter { !$0.isOnline }
-        
-        guard section == 0 else { return offlineUsersCount.count }
-        return onlineUsersCount.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: ConversationCell.identifier,
-            for: indexPath
-        )
-        
-        guard let conversationCell = cell as? ConversationCell else { return cell }
-        
-        if indexPath.section == 0 {
-            let user = users.filter { $0.isOnline }[indexPath.row]
-            conversationCell.configure(with: user)
-        } else {
-            let user = users.filter { !$0.isOnline }[indexPath.row]
-            conversationCell.configure(with: user)
-        }
-        
-        return conversationCell
     }
 }
