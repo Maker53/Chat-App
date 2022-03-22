a1e306f7e4566463325dce473c366ee56a1e580e
Shalgin Stanislav <s.shalgin@gmail.com>
2022-03-22 07:37:51 +0300

    feat(UserProfileInfo): added model for user profile info

---
+++ Chat-App/Profile/Model/UserProfileInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfileInfo.swift
+//  Chat-App
+//
+//  Created by Станислав on 22.03.2022.
+//
+
+import Foundation
+
+class UserProfileInfo: Codable {
+    
+    let name: String?
+    let description: String?
+    let imageURL: URL?
+}
